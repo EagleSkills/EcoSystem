74d983ada7549622b565a1ac737a4a5b9c58073a
Hesham Abdalla <96663605+EagleSkills@users.noreply.github.com>
2023-08-07 21:17:42 +0300

    Adding myproject

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# ## Introductroy
+# in this notebook,A data ecosystem refers to a combination of enterprise infrastructure and applications that are      utilized to aggregate and analyze information
+
+# ### Objectives
+# 
+# * AI-driven modern data ecosystem enables an organization to generate maximum business value from its unique data assets.
+# 
+# * Higher returns. Businesses can generate higher returns with more effective monetization of data and by unlocking value in legacy data stores. 
+# 
+# * Cost savings. Organizations can reduce capital expenses and contain data warehouse costs by transitioning to the cloud—rationalizing and simplifying the data landscape.
+# 
+# * Speed to market and to value. Faster insights through AI-enabled data engineering can reduce time to market.
+# 
+# * Customer engagement. With a modern data ecosystem, businesses can better understand customer and market behavior and use it to enhance their product or service offerings to better meet changing consumer needs and preferences.
+# 
+# * Process improvement. Analyzing big data sets in a modern data ecosystem can optimize and improve internal operations, such as supply chain management and inventory management.
+# 
+# 
+
+# ## list of data science languages:
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1- python
+# 
+# 2- SQL
+# 
+# 3- R
+# 
+# 4- Julia
+# 
+
+# ## list of data science libraries:
+# 
+# Some of the popular liraries that Data Scientists use are:Some of the popular languages that Data Scientists use are:
+# 
+# The popular libraries in python :
+# 
+# 1-for scientific, Pandas and Numpy
+# 
+# 2- for visualization, Matplotlib and seaborn
+# 
+# 3- for ML, Scikit and Scipy and Keras
+# 
+# 4- for deep learning, Tensorflow - Apatche Spark is a general purpose cluster
+# 
+# With R language there are :
+# - Vegas - bigdl - ggplotz - keras - tensorflow
+# 
+
+# ## a table of Data Science tools
+# 
+# |      Data Science Tools      |
+# |------------------------------|
+# |RStudio                       |
+# |                              |
+# |Apatche Spark                 |
+# |                              |
+# |Jupyter notebook              |
+# |                              |
+
+# ### cell introducing arithmetic expression examples
+#     ---------------------------------------------
+
+# In[3]:
+
+
+# cell evaluate the expression ( 3*4) +5
+i = (3*4)+5
+print(i)
+
+
+# In[7]:
+
+
+# convert 200 minutes into hours
+# hours = minute /60
+hours = 200/60
+print(hours)
+
+
+# ## Author
+# 
+# Hesham
